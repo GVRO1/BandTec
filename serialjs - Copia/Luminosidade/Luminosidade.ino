a521b319e182b873e9257c0a6ed195b54319ab19
Aluno <Aluno@Digital.net>
2019-10-07 14:13:17 -0300

    Codigo do node e do html com dados verdadeiros

--- serialjs - Copia/Luminosidade/Luminosidade.ino
+++ serialjs - Copia/Luminosidade/Luminosidade.ino
@@ -1,10 +1,12 @@
-int sensor1 = 100;
+int sensor1 = 0;
 int sensor2 = 150;
 void setup() {
 Serial.begin(9600);
 }
 
 void loop() {
+sensor1 = analogRead(A0);
+sensor2 = analogRead(A1);
 Serial.print(sensor1);
 Serial.print(";");
 Serial.println(sensor2);
